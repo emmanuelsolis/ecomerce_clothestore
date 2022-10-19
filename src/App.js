cf318553845e0d07a6782182f641dc910db7ec79
Emmanuel Solis <emma.ruso@outlook.com>
2022-10-19 11:32:54 -0500

    updating our app with a shop page, coolection preview & collection item component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import './App.css';
-import { Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar.component';
 import Homepage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shoppage';
 
-const HatsPage = (props) => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
-const TopicList = (props) => {
-  return (
-    <div>
-      <h1>TOPIC LIST PAGE</h1>
-      <Link to={"/topics/13"}>To Topic 13</Link>
-      <Link to={"/topics/17"}>To Topic 17</Link>
-      <Link to={"/topics/21"}>To Topic 21</Link>
-    </div>
-  )
-}
-const TopicDetail = ([props, topicId]) => {
-    console.log("TOPICID: ", topicId);
-  return (
-    <div>
-     <h1>tOPIC DETAIL PAGE:{props.topicId}</h1>
-     <h1>tOPIC DETAIL PAGE:{topicId}</h1>
-   </div>
-  )
-}
+
 function App(props) {
   console.log('Las props de App son: ', props);
   return (
@@ -35,9 +13,7 @@ function App(props) {
       {/* <Homepage/> */}
       <Routes>
         <Route exact path='/' element={<Homepage/>}/>
-        <Route exact path='/hats' element={<HatsPage {...props}/>}/>
-        <Route exact path='/topics' element={<TopicList {...props}/>}/>
-        <Route  path='/topics/:topicId' element={<TopicDetail/>}/>
+        <Route exact path='/shop' element={<ShopPage {...props}/>}/>
       </Routes>
     </div>
   );
